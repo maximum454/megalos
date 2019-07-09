ef603099c5a0aa4c5f35ad7bbb445f0ef91af267
maxim <maximum454@gmail.com>
2019-07-09 23:06:11 +0600

    Слайдер на главной

--- src/megalos.js
+++ src/megalos.js
@@ -1,4 +1,5 @@
 import './megalos.scss'
+import './js/slick.min'
 
 import './img/logo.png'
 import './img/logo-group.png'
@@ -8,6 +9,17 @@ import './img/main-company-2.svg'
 import './img/main-company-3.svg'
 import './img/main-company-4.svg'
 import './img/main-company-5.svg'
+import './img/slider-img.jpg'
+
+
+$('.js-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
 
 
 
